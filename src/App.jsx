7faae8212b0934bd8c375d321afcd23b6b19b8c7
Cragt <cntaylor369@gmail.com>
2024-11-01 19:58:37 -0500

    Increase cards displayed to 8

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import Card from "./components/Card/Card";
 import styles from "./App.module.css";
 
 function App() {
+  const cards = [];
+
+  for (let index = 0; index < 8; index++) {
+    cards.push(<Card className={styles.cards} key={index} id={index} />);
+  }
+
   return (
-    <>
-      <div className={styles.page}>
-        <NavBar className={styles.nav} />
-        <Card />
-      </div>
-    </>
+    <div className={styles.page}>
+      <NavBar className={styles.nav} />
+      <div className={styles.cards}>{cards}</div>
+    </div>
   );
 }
 
