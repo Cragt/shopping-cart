c95ebf11b9cfac486b4846112cc14a4cb147e6da
Cragt <cntaylor369@gmail.com>
2024-11-01 19:59:23 -0500

    Fetch contents from api

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,10 +1,26 @@
+import { useEffect, useState } from "react";
 import Button from "../Button/Button";
 import styles from "./Card.module.css";
 
-export default function Card() {
+export default function Card({ id }) {
+  const [item, setItem] = useState(null);
+
+  useEffect(() => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => {
+        setItem(json[id]);
+      })
+      .catch((error) => console.error("Error fetching data:", error));
+  }, [id]);
+
+  if (!item) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className={styles.card}>
-      <div></div>
+      <img className={styles.image} src={item.image} alt={item.title} />
       <Button className={styles.btn} name={"add to cart"} />
     </div>
   );
