0dd5c8f59b92055558938836d664bcbb5686ddea
Cragt <cntaylor369@gmail.com>
2024-11-05 02:41:00 -0600

    Add Cart component for testing

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,29 @@ import "./App.css";
 import NavBar from "./components/Nav Bar/NavBar";
 import Card from "./components/Card/Card";
 import styles from "./App.module.css";
+import ShoppingCart from "./components/Cart/Cart";
 
 function App() {
+  const [itemsInCart, setItemsInCart] = useState([]);
+
   const cards = [];
 
   for (let index = 0; index < 8; index++) {
-    cards.push(<Card className={styles.cards} key={index} id={index} />);
+    cards.push(
+      <Card
+        className={styles.cards}
+        key={index}
+        id={index}
+        setItemsInCart={setItemsInCart}
+      />
+    );
   }
 
   return (
     <div className={styles.page}>
       <NavBar className={styles.nav} />
       <div className={styles.cards}>{cards}</div>
+      <ShoppingCart itemsInCart={itemsInCart} />
     </div>
   );
 }
