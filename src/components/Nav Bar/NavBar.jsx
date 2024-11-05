b6c3b96f272140bef4414ed6cdb68143efa0ab22
Cragt <cntaylor369@gmail.com>
2024-11-05 02:49:23 -0600

    Change nav link names to Home and View Cart

--- src/components/Nav Bar/NavBar.jsx
+++ src/components/Nav Bar/NavBar.jsx
@@ -3,8 +3,8 @@ import styles from "./NavBar.module.css";
 export default function NavBar({ className }) {
   return (
     <nav className={`${styles.navBar} ${className}`}>
-      <a>Example Link</a>
-      <a>Example Link #2</a>
+      <a>Home</a>
+      <a>View Cart</a>
     </nav>
   );
 }
