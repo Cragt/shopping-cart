f17e298e542654b1c2dfde23af85dd5c93fb6419
Cragt <cntaylor369@gmail.com>
2024-11-07 04:12:17 -0600

    Create Home page with header and mickey mouse image

---
+++ src/Home.jsx
@@ -0,0 +1,20 @@
+import NavBar from "./components/Nav Bar/NavBar";
+import styles from "./App.module.css";
+
+export default function Home() {
+  return (
+    <div>
+      <div>
+        <NavBar className={styles.nav} />
+      </div>
+      <div>
+        <h1>Shopping Cart App lol</h1>
+        <img
+          className={styles.mickeyImage}
+          src="src\components\Assets\mickey-mouse.png"
+          alt="Mickey Mouse Lying down"
+        />
+      </div>
+    </div>
+  );
+}
