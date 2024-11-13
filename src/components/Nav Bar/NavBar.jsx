a8dc3a60a787e87df28f6e28d487ff64346394cd
Cragt <cntaylor369@gmail.com>
2024-11-13 03:06:14 -0600

    Fix prop validation error

--- src/components/Nav Bar/NavBar.jsx
+++ src/components/Nav Bar/NavBar.jsx
@@ -1,26 +1,30 @@
-import { useState } from "react";
 import styles from "./NavBar.module.css";
 import { Link } from "react-router-dom";
 import { useCart } from "../../CartContext";
+import PropTypes from "prop-types";
 
 export default function NavBar({ className }) {
-  const { itemsInCart, setItemsInCart } = useCart();
+  const { itemsInCart } = useCart();
   let count = itemsInCart.length;
   return (
     <div>
-    <nav className={`${styles.navBar} ${className}`}>
-      <ul>
-        <li>
-          <Link to={`/`}>Home</Link>
-        </li>
-        <li>
-          <Link to={`/shop`}>Shop</Link>
-        </li>
-        <li>
-          <Link to={`/cart`}>View Cart ({count})</Link>
-        </li>
-      </ul>
-    </nav>
+      <nav className={`${styles.navBar} ${className}`}>
+        <ul>
+          <li>
+            <Link to={`/`}>Home</Link>
+          </li>
+          <li>
+            <Link to={`/shop`}>Shop</Link>
+          </li>
+          <li>
+            <Link to={`/cart`}>View Cart ({count})</Link>
+          </li>
+        </ul>
+      </nav>
     </div>
   );
 }
+
+NavBar.propTypes = {
+  className: PropTypes.string,
+};
